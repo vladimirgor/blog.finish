2fb4a6de73a30172e63f028822494277421b091a
vladimirgor <vladimirgor2011@yandex.ru>
2016-11-28 12:42:42 +0500

    view for users added

--- views/user/show_all.php
+++ views/user/show_all.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Владимир
- * Date: 28.11.2016
- * Time: 12:12
- */
+<?php if ( $users != NULL ):?>
+    <h3>Users list</h3>
+<table class="table table-bordered table-hover table-condensed">
+    <tr>
+        <td><b>id_user</b></td>
+        <td><b>login</b></td>
+        <td><b>id_role</b></td>
+        <td><b>first_name</b></td>
+        <td><b>last_name</b></td>
+        <td><b>action</b></td>
+        </tr>
+    <?php foreach ($users as $user ):?>
+        <tr>
+            <td><?=$user['id_user']?></td>
+            <td><?=$user['login']?></td>
+            <td><?=$user['id_role']?></td>
+            <td><?=$user['first_name']?></td>
+            <td><?=$user['last_name']?></td>
+            <td>edit delete</td>
+        </tr>
+        <?php endforeach; ?>
+</table>
+<?php endif;?>
