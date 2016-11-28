2ce9a5b60a483c471cc1e3ddd35c21b980e17331
vladimirgor <vladimirgor2011@yandex.ru>
2016-11-28 21:02:12 +0500

    user edit view specified

--- views/user/edit.php
+++ views/user/edit.php
@@ -1,7 +1,23 @@
-<?php
+<!--
 /**
  * Created by PhpStorm.
  * User: Владимир
  * Date: 28.11.2016
  * Time: 13:59
  */
+ -->
+<form method="POST">
+    Login:
+    <input type="text"  name="login" value="<?=$user['login']?>"/>
+    <br>
+    Id_role:
+    <input type="number" name="id_role" value="><?=$user['id_role']?>"</>
+    <br>
+    First_name:
+    <input type="text" name="first_name"  value="<?=$user['first_name']?>"</>
+    <br>
+    Last_name:
+    <input type="text" name="last_name"  value="<?=$user['last_name']?>"</>
+    <br>
+    <input type="submit" class="btn btn-primary" value="Save" >
+</form>
