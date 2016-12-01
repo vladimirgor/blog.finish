cb74b93f3f8c9800cee32cea721e662f644885e0
vladimirgor <vladimirgor2011@yandex.ru>
2016-12-01 21:30:59 +0500

    selectors and styles changed

--- views/login.php
+++ views/login.php
@@ -1,20 +1,20 @@
 <h1>Log in</h1>
 
 <form class="form-horizontal" method="post">
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="login" class="col-sm-2 control-label">Login</label>
         <div class="col-sm-10 div_f">
             <input type="text" autofocus required name="login" class="form-control"
                    id="login" value ="<?=$login?>" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="password" class="col-sm-2 control-label">Password</label>
         <div class="col-sm-10 div_f">
             <input type="password" name="password" required class="form-control" id="password" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10 div_f">
             <div class="checkbox div_f">
                 <label>
@@ -23,7 +23,7 @@
             </div>
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10 div_f">
             <button type="submit" class="btn btn-primary">Log in</button>
         </div>
--- views/signing.php
+++ views/signing.php
@@ -1,40 +1,40 @@
 <h1>Register</h1>
 
 <form class="form-horizontal" method="post" onsubmit="return password_match();">
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="first_name" class="col-sm-2 control-label">First name</label>
         <div class="col-sm-10 div_f">
             <input type="text" autofocus required name="first_name" class="form-control"
                    id="login" value ="<?=$first_name?>" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="last_name" class="col-sm-2 control-label">Last name</label>
         <div class="col-sm-10 div_f">
             <input type="text"  required name="last_name" class="form-control"
                    id="login" value ="<?=$last_name?>" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="login" class="col-sm-2 control-label">Login</label>
         <div class="col-sm-10 div_f">
             <input type="text"  required name="login" class="form-control"
                    id="login" value ="<?=$login?>" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="psw" class="col-sm-2 control-label">Password</label>
         <div class="col-sm-10 div_f">
             <input type="password" name="password" required class="form-control" id="psw" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <label for="psw_rep" class="col-sm-2 control-label">Repeat password</label>
         <div class="col-sm-10 div_f">
             <input type="password" name="password" required class="form-control" id="psw_rep" />
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10 div_f">
             <div class="checkbox div_f">
                 <label>
@@ -43,7 +43,7 @@
             </div>
         </div>
     </div>
-    <div class="form-group div_f">
+    <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10 div_f">
             <button type="submit" class="btn btn-primary">Register</button>
         </div>
