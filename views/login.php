63980488e563cb2b3541075e6c523ec5e9c2ef27
vladimirgor <vladimirgor2011@yandex.ru>
2016-12-01 10:39:08 +0500

    bootstrap is implemented to login view

--- views/login.php
+++ views/login.php
@@ -1,9 +1,32 @@
-<h1>Login</h1>
+<h1>Log in</h1>
 
-<form method="post">
-    <input type="text" autofocus required name="login" value ="<?=$login?>" />:Login<br>
-    <input type="password"  required name="password" />:Password<br>
-    <input type="checkbox" checked name="remember" /> Remember me<br>
-    <button type="submit" class="btn btn-primary">Save</button>
+<form class="form-horizontal" method="post">
+    <div class="form-group div_f">
+        <label for="login" class="col-sm-2 control-label">Login</label>
+        <div class="col-sm-10 div_f">
+            <input type="text" autofocus required name="login" class="form-control"
+                   id="login" value ="<?=$login?>" />
+        </div>
+    </div>
+    <div class="form-group div_f">
+        <label for="password" class="col-sm-2 control-label">Password</label>
+        <div class="col-sm-10 div_f">
+            <input type="password" name="password" required class="form-control" id="password" />
+        </div>
+    </div>
+    <div class="form-group div_f">
+        <div class="col-sm-offset-2 col-sm-10 div_f">
+            <div class="checkbox div_f">
+                <label>
+                    <input type="checkbox" checked name="remember"> Remember me
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group div_f">
+        <div class="col-sm-offset-2 col-sm-10 div_f">
+            <button type="submit" class="btn btn-primary">Log in</button>
+        </div>
+    </div>
 </form>
 <span id = "message"><?=$message?></span>
